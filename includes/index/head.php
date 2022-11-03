df6fccbe05d2d4b73fc338f3a8740934e619c298
Blossom147 <111647021+Blossom147@users.noreply.github.com>
2022-11-04 00:36:09 +0700

    chỉnh đường dẫn

--- includes/index/head.php
+++ includes/index/head.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
     <link rel="stylesheet" href="/BTL/css/style.css">
     <link rel="stylesheet" href="/BTL/bootstrap/css/all.min.css">
+    <link rel="stylesheet" href="/BTL/css/dropdown.css">
+
     <!-- <link rel="stylesheet" href="/BTL/css/media.css"> -->
     <title>Music Web Home</title>
 
